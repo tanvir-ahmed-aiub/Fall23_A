b745456cf054a774902eb24e2a028c7cbb786cf7
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-09-27 12:19:48 +0600

    FormandLayout

---
+++ LayoutandForm/LayoutandForm/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutandForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login() { 
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc) {
+            //var temp = Request["Username"];
+            ViewBag.Uname= fc["Username"];
+            ViewBag.Pass = fc["Password"];
+            return View();
+        }
+        public ActionResult LoginSubmit() {
+            //validation
+            //authentication
+            //redirection
+            TempData["Msg"] = "Login Successfull";
+            //return Redirect("https://www.aiub.edu");
+            return RedirectToAction("Index","Dashboard");
+            //return null;
+        }
+    }
+}
---
+++ LayoutandForm/LayoutandForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LayoutandForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LayoutandForm")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7da5c637-cb40-455c-9040-c8fe97ac4a01")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
