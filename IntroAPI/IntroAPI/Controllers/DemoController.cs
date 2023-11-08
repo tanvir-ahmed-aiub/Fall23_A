55ca32fce5d7c830396a417a766ec9b191bc2a4b
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-11-08 14:41:36 +0600

    Intro API

---
+++ IntroAPI/IntroAPI/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/names")]
+        public HttpResponseMessage GetNames() {
+            var names = new string[] { "Tanvir","Sabbir"};
+            return Request.CreateResponse(HttpStatusCode.OK,names);
+        }
+        [HttpPost]
+        [Route("api/test/post")]
+        public HttpResponseMessage ffhh() {
+            return Request.CreateResponse(HttpStatusCode.OK, "Post received");
+        }
+
+        
+    }
+}
